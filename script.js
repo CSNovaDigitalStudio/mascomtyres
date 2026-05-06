f82646c128b9c9362781c228cc267388de00e2eb
CSNovaDigitalStudio <96576667+CSNovaDigitalStudio@users.noreply.github.com>
2026-05-06 13:36:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-document.getElementById("contactForm").addEventListener("submit", function(e){
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (e) {
+
     e.preventDefault();
-    alert("We received your request. We’ll contact you now.");
+
+    const name =
+      document.getElementById("name").value;
+
+    const location =
+      document.getElementById("location").value;
+
+    const issue =
+      document.getElementById("issue").value;
+
+    const message =
+`MASCOM TYRES QUOTE REQUEST
+
+Name: ${name}
+
+Location: ${location}
+
+Issue:
+${issue}`;
+
+    const whatsappURL =
+      `https://wa.me/27787176515?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappURL, "_blank");
 });
